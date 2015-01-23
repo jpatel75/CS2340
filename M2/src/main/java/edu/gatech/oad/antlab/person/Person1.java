d6bae4fed3f1424309c3a834b67ac291c27035f2
Tilak Patel <tilakchandlo@gmail.com>
2015-01-22 19:04:31 -0500

    person 1 - tilak patel antlabmain - p1 - tp

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Tilak Patel
  *  @version 1.1
  */
 public class Person1 {
@@ -29,10 +29,13 @@ public class Person1 {
 	 * @param input the string to be modified
 	 * @return the modified string
 	 */
-	private String calc(String input) {
-	  //Person 1 put your implementation here
-	  return null;
-	}
+    private String calc(String input) {
+        if (input.length() <= 2) {
+            return input;
+        }
+        String front = input.substring(0,2);
+        return input.substring(2) + front;
+    }
 	
 	/**
 	 * Return a string rep of this object
